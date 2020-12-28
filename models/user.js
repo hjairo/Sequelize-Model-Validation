0fe648aa648bbdff894951eaa5321e5c929df243
Jairo <jairoh28@gmail.com>
2020-12-28 17:59:12 -0600

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A name is required',
+          },
+          notEmpty: {
+            msg: 'Please provide a name',
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: 'The email entered already exists',
+        },
+        validate: {
+          notNull: {
+            msg: 'An email is required',
+          },
+          isEmail: {
+            msg: 'Please provide a valid email address',
+          },
+        },
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A birthday is required',
+          },
+          isDate: {
+            msg: 'Birthday must be a valid date',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.VIRTUAL, // set a virtual field
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A password is required',
+          },
+          notEmpty: {
+            msg: 'Please provide a password',
+          },
+          len: {
+            args: [8, 20],
+            msg: 'Password should be between 8 and 20 characters long',
+          },
+        },
+      },
+      confirmedPassword: { // new attribute
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(val) {
+          if (val === this.password) {
+            const hashedPassword = bcrypt.hashSync(val, 10);
+            this.setDataValue('confirmedPassword', hashedPassword);
+          }
+        },
+        validate: {
+          notNull: {
+            msg: 'Both passwords must match',
+          },
+        },
+      },
+    },
+    { sequelize },
+  );
+
+  return User;
+};
